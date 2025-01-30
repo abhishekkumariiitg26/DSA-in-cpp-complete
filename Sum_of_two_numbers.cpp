8008ce98d1fb53efe014d66ebe1f7023cb8265b7
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-01-30 14:56:06 +0530

    initial commit

---
+++ Sum_of_two_numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int sum(int num1,int num2){
+    return num1+num2;
+}
+
+int main(){
+    int num1,num2;
+    cout<<"enter two numbers to add:"<<endl;
+    cin>>num1>>num2;
+    cout<<"The sum of "<<num1<<" and "<<num2<<" is : "<<sum(num1,num2);
+    return 0;
+}
