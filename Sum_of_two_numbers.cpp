a22a0bd5dbca8d33f21b7f4abf6748b68b2661a0
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-01-30 14:57:23 +0530

    added the

--- Sum_of_two_numbers.cpp
+++ Sum_of_two_numbers.cpp
@@ -7,7 +7,7 @@ int sum(int num1,int num2){
 
 int main(){
     int num1,num2;
-    cout<<"enter two numbers to add:"<<endl;
+    cout<<"enter the two numbers to add:"<<endl;
     cin>>num1>>num2;
     cout<<"The sum of "<<num1<<" and "<<num2<<" is : "<<sum(num1,num2);
     return 0;
