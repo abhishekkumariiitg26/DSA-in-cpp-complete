e6111ed6f615b5ad2702b58f84576f60453800c9
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-09 20:23:07 +0530

    initial commit

---
+++ pattern_hollow_square.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int side;
+    cout<<"enter the side length of solid square: ";
+    cin>>side;
+    for(int i=1;i<=side;i++){
+        for (int j = 1; j <= side; j++)
+        {
+            if(i==1 || i==side || j==1 ||j==side){
+
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
