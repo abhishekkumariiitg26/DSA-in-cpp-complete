30515cf52d81a13fe9a62e123525f4e4b9c49fae
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-09 20:16:47 +0530

    initial commit

---
+++ pattern_solid_rectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int length,width;
+    cout<<"enter the length and width of the solid rectangle: ";
+    cin>>length>>width;
+    for(int i=1;i<=length;i++){
+        for (int j = 1; j <= width; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
