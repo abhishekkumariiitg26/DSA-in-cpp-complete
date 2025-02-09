d72c4a85e0e8b8fb943edf9480cdaccb48407f89
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-09 19:10:34 +0530

    initial commit

---
+++ Typecasting.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+    int a=98;
+    char ch='9'; // gives ascii value
+    cout<<a+int(ch)<<endl;
+    float b=4.225;
+    cout<<b<<endl;
+    cout<<int(b)<<endl;
+    return 0;
+}
