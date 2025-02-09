19f19063d23896e39ab799da08aabb1ef417c9b5
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-09 20:12:42 +0530

    changed to variable

--- pattern_1.cpp
+++ pattern_1.cpp
@@ -6,8 +6,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1;i<=4;i++){
-        for(int j=1;j<=4;j++){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
             cout<<j<<" ";
         }
         cout<<endl;
