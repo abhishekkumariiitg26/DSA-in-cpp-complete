ba302bc9a94b2a5921a04f4a30fd59dcb3356fff
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-12 10:23:12 +0530

    initial commit

---
+++ majority_element_optimal_moore_voting_algo.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+
+
+using namespace std;
+
+int Maj_element(vector<int> nums){
+    int freq=0;
+    int ans=0;
+    for(int i=0;i<nums.size();i++){
+        if(freq==0){
+            ans=nums[0];
+        }
+        if(ans==nums[0]){
+            freq++;
+        }
+        else{
+            freq--;
+        }
+    }
+    return ans;
+    
+    
+}
+
+int main(){
+    int size;
+   
+    cout<<"enter the size of the array: ";
+    cin>>size;
+
+    vector<int> nums(size);
+    cout<<"enter the elements of the array in: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin>>nums[i];
+    }
+    cout<<"The elements of the array are: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+   
+    cout<<endl<<"The majority element in this array is: "<<Maj_element(nums);
+  
+    
+    return 0;
+}
