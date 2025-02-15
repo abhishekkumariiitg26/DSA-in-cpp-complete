efbfeab6042b2dd430f325727bc9aaae44e19a7b
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-15 10:50:12 +0530

    formatted

--- Binary_tree_traversals.cpp
+++ Binary_tree_traversals.cpp
@@ -1,94 +1,109 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* left,*right;
-    Node(int val){
-        data=val;
-        left=right=NULL;
+    Node *left, *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
     }
 };
 
-
-void Pre_order(Node* root){
-    if(root==NULL){
-        return ;
+void Pre_order(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
     }
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     Pre_order(root->left);
     Pre_order(root->right);
 }
-void In_order(Node* root){
-    if(root==NULL){
-        return ;
+void In_order(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
     }
     In_order(root->left);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
     In_order(root->right);
 }
-void Post_order(Node* root){
-    if(root==NULL){
-        return ;
+void Post_order(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
     }
     Post_order(root->left);
     Post_order(root->right);
-    cout<<root->data<<" ";
+    cout << root->data << " ";
 }
 
-void Level_order(Node* root){
-    if(root==NULL){
-        return ;
+void Level_order(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
     }
-    queue <Node*> q;
+    queue<Node *> q;
     q.push(root);
 
-
-    
-    Node* temp;
+    Node *temp;
     while (!q.empty())
     {
-        temp=q.front();
+        temp = q.front();
         q.pop();
-        cout<<temp->data<<" ";
+        cout << temp->data << " ";
 
-        if(temp->left){
+        if (temp->left)
+        {
             q.push(temp->left);
         }
-        if(temp->right){
+        if (temp->right)
+        {
             q.push(temp->right);
         }
     }
-    
 }
 
-Node* BinaryTree(){
+Node *BinaryTree()
+{
     int x;
-    cin>>x;
-    if(x==-1){
+    cin >> x;
+    if (x == -1)
+    {
         return NULL;
     }
-    Node* temp=new Node(x);
-    cout<<"Enter the left child of "<<x<<" :";
-    temp->left=BinaryTree();
-    cout<<"Enter the right child of "<<x<<" :";
-    temp->right=BinaryTree();
+    Node *temp = new Node(x);
+    cout << "Enter the left child of " << x << " :";
+    temp->left = BinaryTree();
+    cout << "Enter the right child of " << x << " :";
+    temp->right = BinaryTree();
     return temp;
 }
 
-int main(){
-    cout<<"Enter the root node: ";
-    Node* root;
-    root=BinaryTree();
-    cout<<endl<<"Pre Order traversal: ";
+int main()
+{
+    cout << "Enter the root node: ";
+    Node *root;
+    root = BinaryTree();
+    cout << endl
+         << "Pre Order traversal: ";
     Pre_order(root);
-    cout<<endl<<"In Order traversal: ";
+    cout << endl
+         << "In Order traversal: ";
     In_order(root);
-    cout<<endl<<"Post Order traversal: ";
+    cout << endl
+         << "Post Order traversal: ";
     Post_order(root);
-    cout << endl << "Level Order traversal: ";
+    cout << endl
+         << "Level Order traversal: ";
     Level_order(root);
 
     return 0;
