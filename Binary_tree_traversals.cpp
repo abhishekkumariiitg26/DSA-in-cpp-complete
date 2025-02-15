4469dfb8b0b15f6c5d65b8feecf0f2ec15c6e7f1
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-15 10:05:50 +0530

    best code for traversal and implementation

---
+++ Binary_tree_traversals.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left,*right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+
+
+void Pre_order(Node* root){
+    if(root==NULL){
+        return ;
+    }
+    cout<<root->data<<" ";
+    Pre_order(root->left);
+    Pre_order(root->right);
+}
+void In_order(Node* root){
+    if(root==NULL){
+        return ;
+    }
+    In_order(root->left);
+    cout<<root->data<<" ";
+    In_order(root->right);
+}
+void Post_order(Node* root){
+    if(root==NULL){
+        return ;
+    }
+    Post_order(root->left);
+    Post_order(root->right);
+    cout<<root->data<<" ";
+}
+
+
+Node* BinaryTree(){
+    int x;
+    cin>>x;
+    if(x==-1){
+        return NULL;
+    }
+    Node* temp=new Node(x);
+    cout<<"Enter the left child of "<<x<<" :";
+    temp->left=BinaryTree();
+    cout<<"Enter the right child of "<<x<<" :";
+    temp->right=BinaryTree();
+    return temp;
+}
+
+int main(){
+    cout<<"Enter the root node: ";
+    Node* root;
+    root=BinaryTree();
+    cout<<endl<<"Pre Order traversal: ";
+    Pre_order(root);
+    cout<<endl<<"In Order traversal: ";
+    In_order(root);
+    cout<<endl<<"Post Order traversal: ";
+    Post_order(root);
+
+    return 0;
+}
