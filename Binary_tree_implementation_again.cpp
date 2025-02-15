b36820c4410d32f185c4eeda199040492d55f944
Abhishek Kumar <abhishek.kumar.iiitg.26@gmail.com>
2025-02-15 10:07:48 +0530

    only implementation using queue

---
+++ Binary_tree_implementation_again.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left, *right;
+    Node(int value){
+        data=value;
+        left=right=NULL;
+    }
+};
+
+int main(){
+    // building the root node
+    int node;
+    cout<<"enter the root element: ";
+    cin>>node;
+    int first,second;
+    queue<Node*> q;
+    Node* root=new Node(node);
+    q.push(root);
+
+    // building the binary tree
+    while (!q.empty())
+    {
+        Node* temp=q.front();
+        q.pop();
+        cout<<"enter the left child of "<<temp->data;
+        cin>>first;
+        if(first!=-1){
+            temp->left=new Node(first);
+            q.push(temp->left);
+        }
+        cout<<"enter the right child of "<<temp->data;
+        cin>>second;
+        if(second!=-1){
+            temp->right=new Node(second);
+            q.push(temp->right);
+        }
+    }
+    
+    return 0;
+}
